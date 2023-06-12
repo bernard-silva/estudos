fb55044805383dc3a8240dd61013f6a9c9a9100c
bernard-silva <>
2023-06-12 10:30:44 -0300

    Bloco e Sentença de Código - Leonardo Leitão

---
+++ javascript/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log(                    // 1ª 
+    "Sentença de código")       // Sentença do código
+
+{                               // 1º Bloco de código
+    {                           // 2º Bloco de código
+        console.log("Olá");     // 2ª Sentença do código
+        console.log('Mundo!')   // 3ª Sentença do código
+    }
+}
