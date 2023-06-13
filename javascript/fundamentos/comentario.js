30a5767cb36cf11eda957491fbeaec96d6b7791b
bernard-silva <>
2023-06-13 19:52:40 -0300

    Tipos de Comentários

--- javascript/fundamentos/comentario.js
+++ javascript/fundamentos/comentario.js
@@ -1,12 +1,12 @@
 // Tipos de comentários no Javascript e conceitos
 
 /*
-    1 - Evite comentários óbvios
+    1 - Evite comentários óbvios.
     2 - Conforme conceito do Domain-Driven Design (DDD), use uma linguagem Ubíqua, onde o Analista de Negócios fala
-        os nomes e o Analista Técnico mantem esses nomes no código, mantendo assim a boa comunicação e entendimento
-    3 - No contexto empresarial prese por um código claro e limpo ao invés de ficar comentando tudo
+        os nomes e o Analista Técnico mantém esses nomes no código, formando assim uma boa comunicação e entendimento.
+    3 - No contexto empresarial prese por um código claro e limpo ao invés de ficar comentando tudo.
         Porém enquanto estuda tente sempre comentar o código que está fazendo até mesmo para saber exprimir
-    em palavras o que seu código está fazendo, é uma força de fixar o conhecimento
+        em palavras o que seu código está fazendo, é uma boa forma de fixar o conhecimento.
 */
 
 /*
