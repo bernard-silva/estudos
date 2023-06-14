8ad30b5e9c319b49ad3d54e95210ea7678fbb7aa
bernard-silva <>
2023-06-13 21:48:53 -0300

    Tipagem Fraca

---
+++ javascript/fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer) // string
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer) // mesma variável, agora number
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso ... kkk
+
