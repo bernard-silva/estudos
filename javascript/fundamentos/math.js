b08dbf0998af125e979d8928f7258c5e3e9710f4
bernard-silva <bernard-silva@users.noreply.github.com>
2023-06-16 19:55:37 -0300

    Usando Math

---
+++ javascript/fundamentos/math.js
@@ -0,0 +1,8 @@
+// Na API do JS tem vários Objetos, um deles é o Math
+// como  JS não tem um operador de exponenciação usamos a classe Math
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) // .PI é uma constante no Math e o .pow recebe 1º paramêtro é a base, 2º é o expoente 
+
+console.log(area)
+console.log(typeof Math) // retorna object
