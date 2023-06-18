0f0c3bf301f627e88ba3847fa58ce9e20dcdf576
bernard-silva <bernard-silva@users.noreply.github.com>
2023-06-17 23:34:39 -0300

    Number: Alguns Cuidados

--- javascript/fundamentos/numerosAlgunsCuidados.js
+++ javascript/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0)                  // retorna Infinity
 console.log("10" / 2)               // mesmo sendo string retorna 5
+console.log("3" + 2)                // string ganha, concatena e retorna 32
+console.log("3" - 2)                // ocorre a subtração, retorna 1
 console.log("Show!" * 2)            // algumas linguagens exibem 2 vezes a string, porém em JS retorna NaN
 console.log(0.1 + 0.7)              // retorna 0.799... ao invés de 0.8 devido a especificação IEEE sobre ponto flutuante, 
                                     // onde é mais rapido o processamento e a MAIORIA DAS LINGUAGENS SEGUEM, mesmo não sendo totalmente precisa
